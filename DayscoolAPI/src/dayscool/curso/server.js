128a119e6c4f25a3ed33ec50c4e5a2c38cb585ac
draco1006 <mbarragani@unal.edu.co>
2020-10-16 14:39:38 -0500

    porfavor que no explote

--- DayscoolAPI/src/dayscool/curso/server.js
+++ DayscoolAPI/src/dayscool/curso/server.js
@@ -1,3 +1,3 @@
-export const url = 'ec2-3-231-224-214.compute-1.amazonaws.com'
+export const url = 'ec2-3-234-214-224.compute-1.amazonaws.com'
 export const port = '8000'
 export const entryPoint = 'curso'
--- DayscoolAPI/src/dayscool/messages/resolvers.js
+++ DayscoolAPI/src/dayscool/messages/resolvers.js
@@ -1,4 +1,4 @@
-import { generalRequest, getRequest } from '../../utilities';
+import { generalRequest } from '../../utilities';
 //Añadir la url correspondiente a su microservicio
 import { url, port, entryPoint } from './server';
 const URL = `http://${url}:${port}/${entryPoint}`;
@@ -16,12 +16,84 @@ const resolvers = {
 		createConversation: (_, { idUs, conversation }) =>
 			generalRequest(`${URL}/${idUs}`, 'POST', conversation),
 		createMessage: (_, { idUs,idConv, message }) =>
-			generalRequest(`${URL}/${idUs}/${idConv}`, 'POST', message),
+			create_message(idUs, idConv, message),
 		deleteConversation: (_, { idUs, idConv}) =>
 			generalRequest(`${URL}/${idUs}/${idConv}`, 'DELETE'),
 		deleteMessage: (_, { idUs, idConv, idMsg}) =>
 			generalRequest(`${URL}/${idUs}/${idConv}/${idMsg}`, 'DELETE')
 	}
 };
+
+
+async function rest_message(idUs, idConv, intext) {
+	
+    //Fecha actual para creación de mensaje y notificación
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+	//QUERY Crear un mensaje con parametros
+	let message = {
+		conversationId: idConv,
+		text: intext,
+		sendDate: date,
+		remitentId: idUs
+	}
+	return await generalRequest(`${URL}/${idUs}/${idConv}`, 'POST', message)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.data.createMessage);
+            return data.data.createMessage;
+        });
+}
+
+async function crear_notificacion(idUs, idConv, text, idDestinatario) {
+	var opts = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: ""
+	};
+
+	var query = `mutation {
+		createNotification(notification: {
+			userId: ${idDestinatario},
+			conversationId: ${idConv},
+			message:"${text}",
+			senderId: ${idUs}
+			}) {
+			userId
+			conversationId
+			message
+			senderId
+			}
+			}`;
+	opts["body"] = JSON.stringify({ query });
+	//Crear la notificación
+	await fetch(url_notification, opts)
+		.then(res => res.json())
+		.then(data => { console.log(data.data) });
+}
+async function create_message(idUs, idConv, text) {
+	
+    var message = await rest_message(idUs, idConv, text);
+	//Variable para guardar el destinatario
+    var idDestinatario = await generalRequest(`${URL}/${idUs}`, 'GET')
+        .then(res => res.json())
+        .then(data => {
+            //Filtrar la respuesta a la conversación del mensaje que se creo
+            let newArray = data.data.allConversations.filter(conversation => conversation.id === idConv);
+            //Obtener el destinatario segun el ususario emisor del mensaje
+            if (newArray[0].usuario1Id === idUs) {
+                idDestinatario = newArray[0].usuario2Id;
+            } else {
+                idDestinatario = newArray[0].usuario1Id;
+            }
+            return idDestinatario
+        });
+
+    console.log(idDestinatario);
+    await crear_notificacion(idUs, idConv, text, idDestinatario);
+    return message;
+}
+
+
 
 export default resolvers;
